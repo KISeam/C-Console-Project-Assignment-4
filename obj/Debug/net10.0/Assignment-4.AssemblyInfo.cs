8451eff37fa7f3406ead9bdb3d0c8ac02e86192f
Md. Seam <seam5559@gmail.com>
2026-05-27 14:22:33 +0600

    Create gitignore file

--- obj/Debug/net10.0/Assignment-4.AssemblyInfo.cs
+++ obj/Debug/net10.0/Assignment-4.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Assignment-4")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+015b3fd1d1d325df390fd61a791fa3cb32313a90")]
 [assembly: System.Reflection.AssemblyProductAttribute("Assignment-4")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Assignment-4")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
